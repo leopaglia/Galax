9e05f9e59e0ca5fde2e1ab6673ce7300d0e30f25
leopaglia <paglialungalf@gmail.com>
2015-03-10 17:32:36 -0300

    //--

--- Assets/Scripts/Drop/stdDrop.cs
+++ Assets/Scripts/Drop/stdDrop.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class stdDrop : MonoBehaviour {
 
+    public int speedBuff;
+
     void OnTriggerEnter(Collider other) {
-        
         if(other.tag == "Player" || other.tag == "Weapon"){ //horrible, cambiar
             GameObject.Find("Ship").SendMessage("Buff", speedBuff);
             Destroy(gameObject);
         }
-
     }
 
     void OnBecameInvisible() {
--- Assets/Scripts/Enemies/stdEnemy.cs
+++ Assets/Scripts/Enemies/stdEnemy.cs
@@ -9,10 +9,6 @@ public class stdEnemy : MonoBehaviour {
     public GameObject drop;
 
     private bool isQuitting;
-
-	void Start () {
-	
-	}
 	
 	void Update () {
         
